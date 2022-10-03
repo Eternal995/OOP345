75f439438dc8b06c9dec21d3dbd9b265087422b7
Yongda Long <ylong9@myseneca.ca>
2022-10-02 22:20:59 -0400

    Update Workshop 03

--- workshop03/UniqueQueue.h
+++ workshop03/UniqueQueue.h
@@ -1,3 +1,15 @@
+/*
+*****************************************************************************
+Full Name : Yongda Long
+Student ID# : 172800211
+Email : ylong9@myseneca.ca
+Section : OOP345NBB
+
+I have done all the coding by myself and only copied the code that
+my professor provided to complete my workshops and assignments.
+*****************************************************************************
+*/
+
 #ifndef _SDDS_UNIQUEQUEUE_H_
 #define _SDDS_UNIQUEQUEUE_H_
 
@@ -10,6 +22,8 @@ namespace sdds {
     class UniqueQueue : public Queue<T, 100> {
       public:
         UniqueQueue() {}
+
+        ~UniqueQueue(){};
 
         bool push(const T& item) {
             for (unsigned int i = 0; i < this->size(); i++) {
