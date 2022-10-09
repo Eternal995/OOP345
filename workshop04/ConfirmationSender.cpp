d454c1481b3c519bb5c4aaaaa8969a52f6de9b95
Yongda Long <ylong9@myseneca.ca>
2022-10-09 17:34:04 -0400

    Update Workshop 04

--- workshop04/ConfirmationSender.cpp
+++ workshop04/ConfirmationSender.cpp
@@ -1,3 +1,15 @@
+/*
+*****************************************************************************
+Full Name : Yongda Long
+Student ID# : 172800211
+Email : ylong9@myseneca.ca
+Section : OOP345NBB
+
+I have done all the coding by myself and only copied the code that
+my professor provided to complete my workshops and assignments.
+*****************************************************************************
+*/
+
 #include "ConfirmationSender.h"
 
 namespace sdds {
